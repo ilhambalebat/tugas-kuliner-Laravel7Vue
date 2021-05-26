159ecb1222bb7264fbdbeefc2018d3685792fc77
ilhambalebat <ilhambalebat@gmail.com>
2021-05-26 22:52:55 +0700

    #2 Setup templatenya dulu

--- blog/src/router/index.js
+++ blog/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/auth/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -11,18 +12,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'auth.login',
+    // component: () => import(/* webpackChunkName: "login" */ '../views/auth/Login.vue')
+    component: Login
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
+  linkActiveClass: 'active',
   routes
 })
 
---
+++ blog/src/views/auth/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col-md-6">
+              <div class="card">
+                  <div class="card-header">
+                    Login
+                  </div>
+                  <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input class="form-control" type="email" name="email" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input class="form-control" type="password" name="password" id="password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                  </div>
+              </div>
+
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
